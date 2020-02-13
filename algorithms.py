1050c65ccc3311e936ddb9fbfe7223471a24027b
fatima AlSaadeh <fatiemahsaadeh993@gmail.com>
2020-02-13 01:42:25 -0500

    Repeated, forward, backward and adaptive

--- algorithms.py
+++ algorithms.py
@@ -2,87 +2,175 @@ import heapq
 import sys
 
 from utils import *
+from utils import Node
 
-global adapt_counter
-global goal
 global reached
 
 
-def neighbors(grid, gridInfo, start):
-    (x, y) = (start.x, start.y)
-    points = [(x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)]
+class Algorithms:
+    grid = None
+    grid_info = None
+    open_set = []
+    counter = 0
+    delta = []
+    goal = None
+    start = None
 
-    i = 0
-    k = 8
-    while i < k:
-        # check point is in bounds and not blocked
-        if points[i][0] < 0 and points[i][0] >= 101 and points[i][1] < 0 and points[i][1] >= 101:
-           points.remove(points[i])
-           i -= 1
-           k -= 1
-        else :
-            if(not isinstance(gridInfo[points[i][1]][points[i][0]], Node)):
-              gridInfo[points[i][1]][points[i][0]] = Node()
+    def manhattan_distance(self, start, goal):
+        return abs(start.x - goal.x) + abs(start.y - goal.y)
 
-        i += 1
+    def neighbors(self, start):
+        (x, y) = (start.x, start.y)
+        points = [(x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)]
+        i = 0
+        k = 8
+        while (i < k):
+            if (points[i][0] < 0 and points[i][0] >= 101 and points[i][1] < 0 and points[i][1] >= 101):
+                points.remove(points[i])
+                i -= 1
+                k -= 1
+            else:
+                if (not isinstance(self.grid_info[points[i][1]][points[i][0]], Node)):
+                    self.grid_info[points[i][1]][points[i][0]] = Node()
 
-    # print points
-    return points
+            i += 1
+        return points
 
+    def compute_path(self, tie_break):
+        while self.open_set and self.goal.g > self.open_set[0].f:
+            current = heapq.heappop(self.open_set)
+            for next in self.neighbors(current):
+                if next.search < self.counter:
+                    next.g = sys.maxsize
+                    next.search = self.counter
+                if next.g > current.g + (next.is_blocked if sys.maxsize else 1):
+                    next.g = current.g + (next.is_blocked if sys.maxsize else 1)
+                    next.parent = current
+                    if next in self.open_set:
+                        self.open_set.remove(next)
 
-def adaptastar(grid, gridInfo, start_x, start_y, goal_x, goal_y):
-    start = gridInfo[start_x][start_y]
-    goal = gridInfo[goal_x][goal_y]
-    neighbors(start, grid, gridInfo)
+                    next.h = self.heuristic(self.goal, next)
+                    next.f = (tie_break * next.h) - next.g
+                    heapq.heappush(self.open_set, next)
 
-    path = {}
-    cost = {}
+    def initialize_node(self, node):
+        if (node.search != self.counter and node.search != 0):
+            if (node.g + node.h < node.g):
+                node.h = self.goal.g - node.g
+            node.h = node.h - self.delta[self.counter] - self.delta[node.search]
+            node.h = max(manhattan_distance(node, self.goal), node.h)
+            node.g - sys.maxsize
+        elif (node.search == 0):
+            node.h = manhattan_distance(node, self.goal)
+            node.g = sys.maxsize
+        node.search = self.counter
 
-    cost[start] = manhattan_distance(start, goal)
-    path[start] = None
-    reached = False
+    def move(self, path_end, start_node, goal_node, isBackward=False):
+        if not isBackward:
+            while path_end.parent != start_node and path_end != self.goal:
+                path_end = path_end.parent
+            start_node = path_end
+        else:
+            path_prev = goal_node.parent
+            while path_prev.parent != None and path_end != self.start:
+                path_prev = path_prev.parent
 
-    # while not openSet[0] is None:
-    while start.x != goal.x and start.y != goal.y:
-        openSet = []
-        visited = []
-        adapt_counter+1
-        start.g = 0
-        start.h = cost[start]
-        heapq.heappush(openSet, (0, start))
-        visited.append(start)
-        while openSet:
-            current = heapq.heappop(openSet)
-            if current.x == goal.x and current.y == goal.y:
-                reached = True
+        while self.start != None:
+            prev = start_node
+            cur = self.grid[self.start.x][self.start.y]
+            if cur.is_blocked:
+                cur = prev
+                self.start = self.grid[cur.x][cur.y]
                 break
-            for next in neighbors(current):
-                costCur = cost[current[1]] + manhattan_distance(grid, gridInfo, current[1], next)
-                node = gridInfo[next[1]][next[0]]
-                if next not in cost or costCur < cost[next]:
-                    cost[next] = costCur
-                    node.h = heuristic(goal, next, 4)
-                    node.g = costCur
-                    node.f = node.g + node.h
-                    heapq.heappush(openSet, (node.f, next))
-                    path[next] = current[1]
-                    visited.append(next)
-    curP = goal
-    pathOrder = []
-    while curP is not None and reached:
-        x, y = curP
-        pathOrder.insert(0, (x, y, gridInfo[y][x].h, gridInfo[y][x].g, gridInfo[y][x].f))
-        curP = path[curP]
-    if reached:
-        return reached, pathOrder[::-1], cost[goal], len(cost)
-    return reached, pathOrder[::-1], sys.maxsize, len(cost)
+            if cur == goal_node:
+                self.start = self.grid[cur.x][cur.y]
+                cur.parent = prev
+                break
+            if cur != start_node and prev != cur and cur.parent == None:
+                cur.parent = prev
+            self.start = self.start.next
+        return cur
 
+    def adaptive_astar(self, start_node, goal_node):
+        reached = True
+        self.delta = []
+        self.counter = 0
+        self.start = start_node
+        self.goal = goal_node
+        self.delta.append(self.counter)
+        current_node = self.start
+        while start_node != goal_node:
+            self.open_set = []
+            self.counter = self.counter + 1
+            self.initialize_node(current_node)
+            self.initialize_node(self.goal)
+            current_node.g = 0
+            heapq.heappush(self.open_set, current_node)
+            self.compute_path(1.5)
+            if not self.open_set:
+                print("blocked target")
+                reached = False
+                break
+            path_start = heapq.heappop(self.open_set)
+            cur = self.move(path_start, current_node, goal_node, )
+            if cur is None:
+                break
+            start_node = cur
+        if reached:
+            return start_node
 
-
-def repeated_forward_astar():
-    return None
-
-
-def repeated_backward_astar():
-    return None
-
+    def repeated_astar(self, start_node, goal_node, is_forward, is_backward):
+        reached = True
+        self.counter = 0
+        self.start = start_node
+        self.goal = goal_node
+        self.open_set = []
+        if (is_forward):
+            start_node.g = 0
+            start_node.h = manhattan_distance(start_node, goal_node)
+            start_node.f = start_node.g + start_node.h
+            current_node = self.start
+            while current_node != goal_node:
+                self.open_set = []
+                self.counter = self.counter + 1
+                current_node.g = 0
+                current_node.search = self.counter
+                goal_node.search = self.counter
+                goal_node.g = sys.maxsize
+                heapq.heappush(self.open_set, current_node)
+                self.compute_path(1.9)
+                if not self.open_set:
+                    print("blocked target")
+                    reached = False
+                    break
+                path_start = heapq.heappop(self.open_set)
+                cur = self.move(path_start, current_node, goal_node)
+                if cur is None:
+                    break
+                current_node = cur
+            if reached:
+                return current_node
+        elif (is_backward):
+            start_node.g = 0
+            start_node.h = manhattan_distance(start_node, goal_node)
+            start_node.f = start_node.g + start_node.h
+            current_node = self.start
+            while current_node != goal_node:
+                self.open_set = []
+                self.counter = self.counter + 1
+                current_node.g = 0
+                goal_node.search = self.counter
+                goal_node.g = sys.maxsize
+                heapq.heappush(self.open_set, current_node)
+                self.compute_path(1.5)
+                if not self.open_set:
+                    print("blocked target")
+                    reached = False
+                    break
+                path_end = heapq.heappop(self.open_set)
+                cur = self.move(path_end, current_node, goal_node)
+                if cur is None:
+                    break
+                current_node = cur
+            if reached:
+                return current_node
--- utils.py
+++ utils.py
@@ -3,65 +3,18 @@ from algorithms import *
 
 
 class Node:
-    # 0 = blocked, 1 = regular unblocked, 2 = hard to traverse
-    # a = regular unblocked cell with highway
-    # b = hard to traverse with highway
-    # default settings
     def __init__(self, is_blocked=False):
-        # 0 = blocked, 1 = regular unblocked
-        blockType = "0"
-        self.top = False
-        self.bottom = False
-        self.left = False
-        self.right = False
+        self.is_blocked = is_blocked
         self.parent = None
-        self.child = None
+        self.next = None
         self.x = 0
         self.y = 0
         self.h = 0
         self.g = 0
         self.f = 0
+        self.search=0
 
-
-class Heap:
-    def __init__(self):
-        self
-
-
-# |x1 - x2| + |y1 - y2|
 def manhattan_distance(parent, current):
     (x1, y1) = parent
     (x2, y2) = current
     return (abs(x1 - x2) + abs(y1 - y2))
-
-
-def heuristic(self, goal, current, hType):
-    (x1, y1) = goal
-    (x2, y2) = current
-
-    dx = abs(x1 - x2)
-    dy = abs(y1 - y2)
-
-    # diagonal distance, assuming highways
-    if hType == 0:
-        return .25 * (dx + dy) + (math.sqrt(2) - 2 * .25) * min(dx, dy)
-
-    # straight line distance, assuming highways
-    if hType == 1:
-        return math.sqrt(dx ** 2 + dy ** 2) / 4
-
-    # Manhattan distance, assuming highways
-    if hType == 2:
-        return .25 * (dx + dy)
-
-    # straight line, assuming regular
-    if hType == 3:
-        return math.sqrt(dx ** 2 + dy ** 2)
-
-    # Manhattan, assuming regular
-    if hType == 4:
-        return (dx + dy)
-
-    # diagonal, assuming regular
-    if hType == 5:
-        return (dx + dy) + (math.sqrt(2) - 2) * min(dx, dy)
