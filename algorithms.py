1f062dc485dbcf8e3d6474cdce704a78b7a30a20
fatima AlSaadeh <fatiemahsaadeh993@gmail.com>
2020-02-16 19:59:38 -0500

    Fixed Adaptive Astar

--- algorithms.py
+++ algorithms.py
@@ -1,69 +1,73 @@
 import heapq
 import sys
 
-from utils import *
-from utils import Node
 
-global reached
+class Node:
+    def __init__(self, is_blocked=False):
+        self.is_blocked = is_blocked
+        self.parent = None
+        self.next = None
+        self.x = 0
+        self.y = 0
+        self.h = 0
+        self.g = 0
+        self.f = 0
+        self.search = 0
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+    def add_child(self, node):
+        temp = self.next
+        self.next = node
+        node.next = temp
 
 
-class Algorithms:
-    grid = None
-    grid_info = None
-    open_set = []
-    counter = 0
-    delta = []
-    goal = None
-    start = None
+class Algorithm:
+    def __init__(self):
+        self.grid = None
+        self.grid_info = None
+        self.open_set = {}
+        self.counter = 0
+        self.delta = []
+        self.goal = None
+        self.start = None
+        self.reached = None
 
     def manhattan_distance(self, start, goal):
         return abs(start.x - goal.x) + abs(start.y - goal.y)
 
     def neighbors(self, start):
-        (x, y) = (start.x, start.y)
+        neighborslist = []
+        (x, y) = start
         points = [(x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)]
         i = 0
-        k = 8
+        k = 4
         while (i < k):
-            if (points[i][0] < 0 and points[i][0] >= 101 and points[i][1] < 0 and points[i][1] >= 101):
+            if (points[i][0] < 0 or points[i][0] >= 101 or points[i][1] < 0 or points[i][1] >= 101):
                 points.remove(points[i])
                 i -= 1
                 k -= 1
             else:
                 if (not isinstance(self.grid_info[points[i][1]][points[i][0]], Node)):
                     self.grid_info[points[i][1]][points[i][0]] = Node()
+                neighborslist.append(self.grid_info[points[i][1]][points[i][0]])
 
             i += 1
         return points
 
-    def compute_path(self, tie_break):
-        while self.open_set and self.goal.g > self.open_set[0].f:
-            current = heapq.heappop(self.open_set)
-            for next in self.neighbors(current):
-                if next.search < self.counter:
-                    next.g = sys.maxsize
-                    next.search = self.counter
-                if next.g > current.g + (next.is_blocked if sys.maxsize else 1):
-                    next.g = current.g + (next.is_blocked if sys.maxsize else 1)
-                    next.parent = current
-                    if next in self.open_set:
-                        self.open_set.remove(next)
-
-                    next.h = self.heuristic(self.goal, next)
-                    next.f = (tie_break * next.h) - next.g
-                    heapq.heappush(self.open_set, next)
-
-    def initialize_node(self, node):
-        if (node.search != self.counter and node.search != 0):
-            if (node.g + node.h < node.g):
-                node.h = self.goal.g - node.g
-            node.h = node.h - self.delta[self.counter] - self.delta[node.search]
-            node.h = max(manhattan_distance(node, self.goal), node.h)
-            node.g - sys.maxsize
-        elif (node.search == 0):
-            node.h = manhattan_distance(node, self.goal)
-            node.g = sys.maxsize
-        node.search = self.counter
+    def initialize_node(self, cur_node, delta, counter):
+        cur_node_o = self.grid_info[cur_node[1]][cur_node[0]]
+        if (cur_node_o.search != self.counter and cur_node_o.search != 0):
+            if (cur_node_o.g + cur_node_o.h < cur_node_o.g):
+                cur_node_o.h = self.goal.g - cur_node_o.g
+            cur_node_o.h = max(self.manhattan_distance(cur_node_o, self.goal), cur_node_o.h)
+            cur_node_o.g = sys.maxsize
+        elif (cur_node_o.search == 0):
+            cur_node_o.h = self.manhattan_distance(cur_node_o, self.goal)
+            cur_node_o.g = sys.maxsize
+        cur_node_o.search = counter
+        self.grid_info[cur_node[1]][cur_node[0]] = cur_node_o
 
     def move(self, path_end, start_node, goal_node, isBackward=False):
         if not isBackward:
@@ -91,68 +95,125 @@ class Algorithms:
             self.start = self.start.next
         return cur
 
-    def adaptive_astar(self, start_node, goal_node):
-        reached = True
-        self.delta = []
-        self.counter = 0
-        self.start = start_node
-        self.goal = goal_node
-        self.delta.append(self.counter)
-        current_node = self.start
-        while start_node != goal_node:
-            self.open_set = []
-            self.counter = self.counter + 1
-            self.initialize_node(current_node)
-            self.initialize_node(self.goal)
-            current_node.g = 0
-            heapq.heappush(self.open_set, current_node)
-            self.compute_path(1.5)
-            if not self.open_set:
-                print("blocked target")
-                reached = False
-                break
-            path_start = heapq.heappop(self.open_set)
-            cur = self.move(path_start, current_node, goal_node, )
-            if cur is None:
-                break
-            start_node = cur
-        if reached:
-            return start_node
 
-    def repeated_astar(self, start_node, goal_node, is_forward, is_backward):
-        reached = True
-        self.counter = 0
-        self.start = start_node
-        self.goal = goal_node
-        self.open_set = []
+    def adaptive_astar(self, tie_break):
+        start = (self.start.x, self.start.y)
+        goal = (self.goal.x, self.goal.y)
+        open_set = []
+        closed_set = []
+        path = {}
+        cost = {}
+
+        counter = 0
+        delta = []
+        delta.append(counter)
+
+        heapq.heappush(open_set, (0, start))
+        path[start] = None
+        cost[start] = 0
+        reached = False
+
+        self.grid_info[start[1]][start[0]].g = 0
+        self.grid_info[start[1]][start[0]].h = self.manhattan_distance(self.grid_info[start[1]][start[0]], self.grid_info[goal[1]][goal[0]])
+        self.grid_info[start[1]][start[0]].f = self.grid_info[start[1]][start[0]].g + self.grid_info[start[1]][start[0]].h
+
+        current = heapq.heappop(open_set)[1]
+        while current != goal:
+            self.initialize_node(current, delta, counter)
+            self.initialize_node(goal, delta, counter)
+            self.grid_info[current[1]][current[0]].g = 0
+            for next in self.neighbors(current):
+                cur_cost = cost[current] + (sys.maxsize if self.grid_info[next[1]][next[0]].is_blocked else 1)
+                node = self.grid_info[next[1]][next[0]]
+                if next not in cost or cur_cost < cost[next]:
+                    cost[next] = cur_cost
+                    if tie_break != 0:
+                        node.h = self.manhattan_distance(self.grid_info[goal[1]][goal[0]], self.grid_info[next[1]][next[0]])
+                    node.g = cur_cost
+                    node.f = node.g + tie_break * node.h
+                    heapq.heappush(open_set, (node.f, next))
+                    path[next] = current
+            prev = current
+            current = heapq.heappop(open_set)[1]
+            if current == goal:
+                reached = True
+                break
+            else:
+                self.initialize_node(current, delta, counter)
+                if self.grid_info[current[1]][current[0]].g + self.grid_info[current[1]][current[0]].h < self.grid_info[prev[1]][prev[0]].g:
+                    self.grid_info[current[1]][current[0]].h = self.grid_info[goal[1]][goal[0]].g - self.grid_info[current[1]][current[0]].g
+        cur_p = goal
+        pathOrder = []
+        while cur_p is not None and reached:
+            x, y = cur_p
+            pathOrder.insert(0, (x, y, self.grid_info[y][x].h, self.grid_info[y][x].g, self.grid_info[y][x].f))
+            cur_p = path[cur_p]
+        if reached:
+            return reached, pathOrder[::-1], cost[goal], len(cost)
+        return reached, pathOrder[::-1], sys.maxsize, len(cost)
+
+    def repeated_astar(self, is_forward, is_backward, tie_break):
+        start = (self.start.x, self.start.y)
+        goal = (self.goal.x, self.goal.y)
+        open_set = []
+        closed_Set = []
+        path = {}
+        cost = {}
+        counter = 0
+        delta = []
+        delta.append(counter)
+        heapq.heappush(open_set, (0, start))
+        path[start] = None
+        cost[start] = 0
+        reached = False
         if (is_forward):
-            start_node.g = 0
-            start_node.h = manhattan_distance(start_node, goal_node)
-            start_node.f = start_node.g + start_node.h
-            current_node = self.start
-            while current_node != goal_node:
+            self.grid_info[start[1]][start[0]].g = 0
+            self.grid_info[start[1]][start[0]].h = self.manhattan_distance(self.grid_info[start[1]][start[0]], self.grid_info[goal[1]][goal[0]])
+            self.grid_info[start[1]][start[0]].f = self.grid_info[start[1]][start[0]].g + self.grid_info[start[1]][start[0]].h
+            current = heapq.heappop(open_set)[1]
+            while current != goal:
+                counter+=1
+                self.grid_info[current[1]][current[0]].search = counter
+                self.grid_info[current[1]][current[0]].g = 0
+                self.grid_info[current[1]][current[0]].h = self.manhattan_distance(self.grid_info[start[1]][start[0]], self.grid_info[goal[1]][goal[0]])
+                self.grid_info[current[1]][current[0]].f = self.grid_info[current[1]][current[0]].h + self.grid_info[current[1]][current[0]].g
+                self.grid_info[goal[1]][goal[0]].g = sys.maxsize
+                self.grid_info[goal[1]][goal[0]].search = counter
                 self.open_set = []
-                self.counter = self.counter + 1
-                current_node.g = 0
-                current_node.search = self.counter
-                goal_node.search = self.counter
-                goal_node.g = sys.maxsize
-                heapq.heappush(self.open_set, current_node)
-                self.compute_path(1.9)
+                self.closed_set = []
+                heapq.heappush(open_set, (self.grid_info[current[1]][current[0]].f, current))
+                while self.grid_info[goal[1]][goal[0]].g > self.grid_info[current[1]][current[0]].f:
+                    current = heapq.heappop(open_set)[1]
+                    closed_Set.append(current)
+                    for next in self.neighbors(current):
+                        costCur = cost[current] + (
+                            sys.maxsize if self.grid_info[next[1]][next[0]].is_blocked else 1)
+                        node = self.grid_info[next[1]][next[0]]
+                        if next not in cost or costCur < cost[next]:
+                            cost[next] = costCur
+                            if tie_break != 0:
+                                node.h = self.manhattan_distance(self.grid_info[goal[1]][goal[0]],
+                                                                 self.grid_info[next[1]][next[0]])
+                            node.g = costCur
+                            node.f = node.g + tie_break * node.h
+                            heapq.heappush(open_set, (node.f, next))
+                            path[next] = current[1]
                 if not self.open_set:
                     print("blocked target")
                     reached = False
                     break
-                path_start = heapq.heappop(self.open_set)
-                cur = self.move(path_start, current_node, goal_node)
-                if cur is None:
-                    break
-                current_node = cur
+            cur_p = goal
+            pathOrder = []
+            while cur_p is not None and reached:
+                x, y = cur_p
+                pathOrder.insert(0, (x, y, self.grid_info[y][x].h, self.grid_info[y][x].g, self.grid_info[y][x].f))
+                cur_p = path[cur_p]
             if reached:
-                return current_node
+                return reached, pathOrder[::-1], cost[goal], len(cost)
+            return reached, pathOrder[::-1], sys.maxsize, len(cost)
         elif (is_backward):
             start_node.g = 0
-            start_node.h = manhattan_distance(start_node, goal_node)
+            start_node.h = self.manhattan_distance(start_node, goal_node)
             start_node.f = start_node.g + start_node.h
             current_node = self.start
             while current_node != goal_node:
--- utils.py
+++ utils.py
@@ -1,20 +1,167 @@
 import math
+
+import numpy as np
+import matplotlib
+import sys
+import tkinter
+import heapq
+import math
+
+from numpy.distutils.fcompiler import str2bool
+
 from algorithms import *
 
 
-class Node:
-    def __init__(self, is_blocked=False):
-        self.is_blocked = is_blocked
-        self.parent = None
-        self.next = None
-        self.x = 0
-        self.y = 0
-        self.h = 0
-        self.g = 0
-        self.f = 0
-        self.search=0
 
-def manhattan_distance(parent, current):
-    (x1, y1) = parent
-    (x2, y2) = current
-    return (abs(x1 - x2) + abs(y1 - y2))
+BLACK = '#000000'
+DARK_GRAY = '#494949'
+LIGHT_GRAY = '#979899'
+WHITE = '#fff'
+
+class grid:
+	### Metadata
+	gridInfo = [] # numpy array
+	# numpy array(s) holding the integer coordinates
+	start = []
+	goal = []
+	centers = [] # 8 centers for hard to traverse
+
+	### GUI variables
+	numRows = 0
+	numCols = 0
+	sideLength = 15
+	canvas = 0
+
+	def _fillMetaData(self):
+		self.numRows = len(self.gridInfo)
+		self.numCols = len(self.gridInfo[0])
+		for i in range(len(self.gridInfo)):
+			for j in range(len(self.gridInfo[0])):
+				node = self.gridInfo[i][j]
+
+		for i in range(len(self.gridInfo)):
+			for j in range(len(self.gridInfo[0])):
+				node = self.gridInfo[i][j]
+		self.gridInfo = np.asarray(self.gridInfo)
+
+	def _drawCell(self, node, tag, r, c, border):
+		color = DARK_GRAY
+		# Background Color
+		if((r == self.start[0] and c == self.start[1]) or (r == self.goal[0] and c == self.goal[1])):
+			node.is_blocked=False
+		if not node.is_blocked:
+			color = WHITE
+		if node.is_blocked:
+			color = LIGHT_GRAY
+
+		self.canvas.create_rectangle(
+			r*self.sideLength, c*self.sideLength,
+			(r+1)*self.sideLength, (c+1)*self.sideLength,
+			outline=border, fill=color, tag=tag)
+
+		cpt_x = (r*self.sideLength + (r+1)*self.sideLength)/2
+		cpt_y = (c*self.sideLength + (c+1)*self.sideLength)/2
+		# Mark Start and Goal Nodes
+		if (r == self.goal[0] and c == self.goal[1]):
+			self.canvas.create_oval(
+				r*self.sideLength+2,c*self.sideLength+2,
+				(r+1)*self.sideLength-2, (c+1)*self.sideLength-2,
+				fill='red')
+			self.gridInfo[r][c].is_blocked=False
+		if (r == self.start[0] and c == self.start[1]):
+			self.canvas.create_oval(
+				r*self.sideLength+2,c*self.sideLength+2,
+				(r+1)*self.sideLength-2, (c+1)*self.sideLength-2,
+				fill='green')
+			self.gridInfo[r][c].is_blocked=False
+
+
+	def _drawButton(self, root, point):
+		x = point[0]
+		y = point[1]
+		f = tkinter.Frame(root, height=self.sideLength+1, width=self.sideLength+1)
+		f.pack_propagate(0)
+		b = tkinter.Button(f, bg = "red", bd = 1, command = lambda: self._print(point))
+		f.pack()
+		b.pack()
+		f.place(x = x*self.sideLength, y = y*self.sideLength)
+
+	def _print(self, info):
+		x, y = info[0], info[1]
+		node = self.gridInfo[y][x]
+		temp = ""
+		if (x==self.start[0] and y ==self.start[1]):
+			temp = temp + "Start:	"
+		if (x==self.goal[0] and y ==self.goal[1]):
+			temp = temp + "Goal:	"
+		temp = temp + "x="+str(x)+" y="+str(y)
+		print (temp)
+
+	def createGridFromFile(self, file):
+		# print "Extracting information from file..."
+		fp = open(file, 'r')
+		lines = fp.readlines()
+		lines = [line.rstrip('\n') for line in lines]
+
+		# print "Processing start and goal nodes..."
+		start = np.asarray(lines[0].split())
+		self.start = [int(a) for a in start]
+		goal = np.asarray(lines[1].split())
+		self.goal = [int(a) for a in goal]
+
+		# print "Extracting grid data ..."
+		temp = lines[2:]
+		info = []
+		[info.append(line.split()) for line in temp]
+		for i in range(len(info)):
+			gridRow = []
+			for j in range (len(info[0])):
+				x = Node( str2bool(info[i][j]))
+				gridRow.append(x)
+			self.gridInfo.append(gridRow)
+		# print [[a.blockType for a in row] for row in self.gridInfo]
+		self._fillMetaData()
+
+	def createGUI(self, root):
+		if self.numCols > 50 or self.numRows > 50:
+			self.sideLength = 7
+		self.width = self.numCols*self.sideLength
+		self.height = self.numRows*self.sideLength
+		self.canvas = tkinter.Canvas(root, width=self.width, height=self.height)
+		root.wm_title("Map\n")
+		for c in range(self.numRows):
+			for r in range(self.numCols):
+				node  = self.gridInfo[c][r]
+				tag = str(r)+" "+str(c)
+				self._drawCell(node, tag, r, c, BLACK)
+		self.canvas.pack()
+
+	def createButtonGUI(self, root, pathInfo):
+		self.createGUI(root)
+		for i in range(len(pathInfo)):
+			self._drawButton(root, pathInfo[i])
+		self.canvas.pack()
+
+
+grid_o = grid()
+grid_o.createGridFromFile("map0.txt")
+root = tkinter.Tk()
+alg = Algorithm()
+startn= Node()
+startn.x=grid_o.start[0]
+startn.y=grid_o.start[1]
+goaln= Node()
+goaln.x=grid_o.goal[0]
+goaln.y=grid_o.goal[1]
+alg.start =  startn
+alg.goal = goaln
+alg.grid_info=grid_o.gridInfo
+
+r, p, c, numExpanded = alg.repeated_astar(True, False, 1.25)
+if r is not None:
+	grid_o.createButtonGUI(root, p)
+else:
+	grid_o.createGUI(root)
+root.mainloop()
+grid_o.createGUI(root)
+root.mainloop()
