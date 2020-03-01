e25d5834f98a5b4a0702576ba5501054a06c8222
ashdunn <31973458+ashdunn@users.noreply.github.com>
2020-03-01 16:43:09 -0500

    changed adapative

--- algorithms.py
+++ algorithms.py
@@ -77,7 +77,7 @@ class Algorithm:
             self.start = self.start.next
         return cur
 
-    def adaptive_astar(self, tie_break):
+    def adaptive_old(self, tie_break):
         start = (self.start.x, self.start.y)
         goal = (self.goal.x, self.goal.y)
         open_set = []
@@ -141,7 +141,158 @@ class Algorithm:
         if reached:
             end_time = time.time()
             total_time = end_time - start_time
-            #print(numexpanded)
+            # print(numexpanded)
+            if show:
+                r = tk.Tk()
+                grid_o.display_path(r, pathOrder[::-1])
+                r.title("final representation")
+                r.mainloop()
+            # return reached, pathOrder[::-1], self.grid_info[goal[1]][goal[0]].g, len(cost)
+            return total_time, numexpanded
+        return 0, 0
+
+    def adaptive_astar(self, steps, grid_o, tie_break, show):
+
+        open_set = []
+        closed_set = []
+        path = {}
+        cost = {}
+        counter = 0
+        delta = []
+        delta.append(counter)
+        reached = False
+        prev = None
+        final_path = {}
+
+        start = (self.start.x, self.start.y)
+        goal = (self.goal.x, self.goal.y)
+        heapq.heappush(open_set, (0, start))
+        path[start] = None
+        cost[start] = 0
+        snode = self.grid_info[start[1]][start[0]]
+        gnode = self.grid_info[goal[1]][goal[0]]
+        snode.g = 0
+        snode.h = manhattan_distance(snode, gnode)
+        snode.f = snode.g + snode.h
+        numexpanded = 0
+        current = heapq.heappop(open_set)[1]
+        numexpanded = numexpanded + 1
+        start_time = time.time()
+
+        goal_g = 0
+
+        while current != goal:
+            path = {}
+            counter += 1
+            cnode = self.grid_info[current[1]][current[0]]
+            cnode.search = counter
+            cnode.g = 0
+            if goal_g > 0 and cnode.g:
+                cnode.h = goal_g - cnode.g
+            else:
+                cnode.h = manhattan_distance(cnode, gnode)
+            cnode.f = cnode.h + cnode.g
+
+            gnode.g = sys.maxsize
+            gnode.search = counter
+            open_set = []
+            closed_set = [final_path.keys()]
+            heapq.heappush(open_set, (cnode.f, current))
+            while open_set and gnode.g > open_set[0][0]:
+                numexpanded = numexpanded + 1
+                current = heapq.heappop(open_set)[1]
+                cnode = self.grid_info[current[1]][current[0]]
+                closed_set.append(current)
+                for next in self.neighbors(current):
+                    if next not in closed_set:
+                        nextnode = self.grid_info[next[1]][next[0]]
+                        if nextnode.search < counter:
+                            if goal_g > 0 and nextnode.g and nextnode.search == counter-1:
+                                nextnode.h = goal_g - nextnode.g
+                            nextnode.g = sys.maxsize
+                            nextnode.search = counter
+
+                        costCur = cnode.g + (sys.maxsize if nextnode.is_seen else 1)
+                        if nextnode.g is None or costCur < nextnode.g:
+                            if (nextnode.f, next) in open_set:
+                                open_set.remove((nextnode.f, next))
+                                heapq.heapify(open_set)
+
+                            if nextnode.h == 0:
+                                nextnode.h = manhattan_distance(gnode, nextnode)
+                            nextnode.g = costCur
+                            nextnode.f = nextnode.g + nextnode.h
+                            nextnode.f = (tie_break * nextnode.f) - nextnode.g
+                            heapq.heappush(open_set, (nextnode.f, next))
+                            path[next] = current
+            if not open_set:
+                print("blocked target")
+                r = tk.Tk()
+                grid_o.create_maze(r)
+                reached = False
+                break
+            elif goal in path:
+                reached = True
+                goal_g = gnode.g
+            else:
+                goal_g = 0
+
+            cur_p = goal
+            pathOrder = []
+            path = {**path, **final_path}
+
+            while cur_p is not None and reached:
+                x, y = cur_p
+                if any(r.x == x and r.y == y for r in pathOrder):
+                    break
+                pathOrder.insert(0, self.grid_info[y][x])
+                if (cur_p in path):
+                    cur_p = path[cur_p]
+                else:
+                    cur_p = None
+
+            # move
+            located = pathOrder[0]
+            invalid = False
+            for i in range(1, len(pathOrder)):
+                n = self.neighbors((located.x, located.y), True)
+                potential_next = pathOrder[i]
+                if potential_next.is_seen:
+                    if steps:
+                        r = tk.Tk()
+                        grid_o.display_path(r, pathOrder[:i][::-1])
+                        r.title("intermediate representation")
+                        r.mainloop()
+
+                    break
+                else:
+                    # print(final_path)
+                    final_path[(potential_next.x, potential_next.y)] = (located.x, located.y)
+                    located = potential_next
+
+            current = (located.x, located.y)
+
+            # print(current)
+            if current == goal:
+                cur_p = goal
+                pathOrder = []
+                while cur_p is not None and reached:
+                    x, y = cur_p
+                    pathOrder.insert(0, self.grid_info[y][x])
+                    if (cur_p in final_path):
+                        cur_p = final_path[cur_p]
+                    else:
+                        cur_p = None
+                reached = True
+            else:
+                reached = False
+
+            #print("cur: ", current, "goal: ", goal, "reached: ", reached, (self.grid_info[current[1]][current[0]]).g)
+
+        if reached:
+            end_time = time.time()
+            total_time = end_time - start_time
+            print(numexpanded)
             if show:
                 r = tk.Tk()
                 grid_o.display_path(r, pathOrder[::-1])
@@ -214,7 +365,7 @@ class Algorithm:
                                 nextnode.h = manhattan_distance(gnode, nextnode)
                                 nextnode.g = costCur
                                 nextnode.f = nextnode.g + nextnode.h
-                                nextnode.f = (tie_break*nextnode.f) - nextnode.g
+                                nextnode.f = (tie_break * nextnode.f) - nextnode.g
                                 heapq.heappush(open_set, (nextnode.f, next))
                                 path[next] = current
                 if not open_set:
@@ -289,7 +440,7 @@ class Algorithm:
                     grid_o.display_path(r, pathOrder[::-1])
                     r.title("final representation")
                     r.mainloop()
-                #return reached, pathOrder[::-1], self.grid_info[goal[1]][goal[0]].g, len(cost)
+                # return reached, pathOrder[::-1], self.grid_info[goal[1]][goal[0]].g, len(cost)
                 return total_time, numexpanded
             return 0, 0
 
@@ -403,7 +554,8 @@ class Algorithm:
                 else:
                     reached = False
 
-                print("cur: ", current, "goal: ", goal, "reached: ", reached, (self.grid_info[current[1]][current[0]]).g)
+                print("cur: ", current, "goal: ", goal, "reached: ", reached,
+                      (self.grid_info[current[1]][current[0]]).g)
             if reached:
                 if show:
                     r = tk.Tk()
@@ -414,54 +566,39 @@ class Algorithm:
                 total_time = end_time - start_time
                 print(numexpanded)
 
-            #     return reached, pathOrder[::-1], self.grid_info[goal[1]][goal[0]].g, len(cost)
-            # return reached, pathOrder[::-1], sys.maxsize, len(cost)
+                #     return reached, pathOrder[::-1], self.grid_info[goal[1]][goal[0]].g, len(cost)
+                # return reached, pathOrder[::-1], sys.maxsize, len(cost)
                 return total_time, numexpanded
             return 0, 0
 
 
 def main():
-    grid_o = Grid()
-    # root = tk.Tk()
-    alg = Algorithm(grid_o)
     steps = False
     f = True
     b = False
     show = False
-    # r, p, c, num_expanded = alg.adaptive_astar(1.25)
 
     avg_time_repeated_f = 0;
     avg_expanded_repeated_f = 0;
     count = 10
-    #alg.repeated_astar(steps, grid_o, f, b, 1.25, show)
-    #alg.repeated_astar(steps, grid_o, f, b, 12, True)
     for i in range(count):
         grid_o = Grid()
         alg = Algorithm(grid_o)
-        total_time, num_expanded = alg.repeated_astar(steps, grid_o, f, b, 200, show)
-        #total_time, num_expanded = alg.adaptive_astar(grid_o, 0, show)
+        # total_time, num_expanded = alg.repeated_astar(steps, grid_o, f, b, 200, show)
+        total_time, num_expanded = alg.adaptive_astar(steps, grid_o, 200, show)
         if total_time == 0:
-            count = count-1
+            count = count - 1
             continue
         print("expanded", num_expanded)
         print("time", total_time)
         avg_expanded_repeated_f += num_expanded
         avg_time_repeated_f += total_time
 
-    avg_time_repeated_f = avg_time_repeated_f/count
-    avg_expanded_repeated_f = avg_expanded_repeated_f/count
+    avg_time_repeated_f = avg_time_repeated_f / count
+    avg_expanded_repeated_f = avg_expanded_repeated_f / count
 
     print(avg_expanded_repeated_f)
     print(avg_time_repeated_f)
-
-    '''
-    if r is not None:
-        #root.title("final path")
-        grid_o.display_path(root, p)
-    else:
-        grid_o.create_maze(root)
-    root.mainloop()
-    '''
 
 
 if __name__ == "__main__":
